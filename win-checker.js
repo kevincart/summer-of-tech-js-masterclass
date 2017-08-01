680155d3bf3a69bb8708ce5f69ee7b1ca37542b9
Kevin Cartwright <kevincart@gmail.com>
2017-08-01 19:07:03 +1200

    working through game, slide 2/34

--- win-checker.js
+++ win-checker.js
@@ -2,7 +2,14 @@ export default class WinChecker {
     checkWin (gameState) {
         return false;
     }
+    checkWinScenario (winMoves, currentPlayerMoves) {
+    return winMoves.every(move => currentPlayerMoves.indexOf(move) !== -1);
+    }
+    checkPlayerWin (currentPlayerMoves) {
+    return WIN_SCENARIOS.some(winMoves => this.checkWinScenario(winMoves, currentPlayerMoves));
+    }
 }
+
 const WIN_SCENARIOS = [
     [0, 3, 6], // First column
     [1, 4, 7], // Second column
@@ -13,6 +20,3 @@ const WIN_SCENARIOS = [
     [0, 4, 8], // Left-top to right-bottom diagonal
     [2, 4, 6]  // Right-top to left-bottom diagonal
 ];
-checkWinScenario (winMoves, currentPlayerMoves) {
-    return winMoves.every(move => currentPlayerMoves.indexOf(move) !== -1);
-}
